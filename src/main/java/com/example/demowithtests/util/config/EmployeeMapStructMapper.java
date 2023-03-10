a62f1fb2bcf1c56355e29c19571c32fddad92ca3
Константин Довбыш <dokongr@gmail.com>
2023-03-10 20:56:27 +0200

    EmployeeMapStructMapper: disableBuilder = false

--- src/main/java/com/example/demowithtests/util/config/EmployeeMapStructMapper.java
+++ src/main/java/com/example/demowithtests/util/config/EmployeeMapStructMapper.java
@@ -3,9 +3,10 @@ package com.example.demowithtests.util.config;
 import com.example.demowithtests.domain.Employee;
 import com.example.demowithtests.dto.EmployeeDto;
 import com.example.demowithtests.dto.EmployeeReadDto;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false))
 public interface EmployeeMapStructMapper {
     Employee employeeDtoToEmployee(EmployeeDto employeeDto);
     EmployeeDto employeeToEmployeeDto(Employee employee);
