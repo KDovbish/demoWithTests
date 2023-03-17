5b6d7bb3032a99e0b655c165f67db5fb8a50d2bb
Константин Довбыш <dokongr@gmail.com>
2023-03-17 11:13:36 +0200

    mapper: logic is added for save employee to db

--- src/main/java/com/example/demowithtests/util/config/EmployeeMapStructMapper.java
+++ src/main/java/com/example/demowithtests/util/config/EmployeeMapStructMapper.java
@@ -1,14 +1,31 @@
 package com.example.demowithtests.util.config;
 
+import com.example.demowithtests.domain.Address;
 import com.example.demowithtests.domain.Employee;
-import com.example.demowithtests.dto.EmployeeDto;
-import com.example.demowithtests.dto.EmployeeReadDto;
+import com.example.demowithtests.domain.Photo;
+import com.example.demowithtests.dto.*;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", builder = @Builder(disableBuilder = false))
 public interface EmployeeMapStructMapper {
+
+    //  Логика создания сущностей из DTO для сохранения в БД
+    @Mapping(target = "isVisible", constant = "true")
+    Employee employeeCreateDtoToEmployee(EmployeeCreateDto employeeCreateDto); // исходное свойство в dto отсутствует; целевое прописывается в любом случае
+
+    @Mapping(target = "addDate", expression = "java( java.util.Date.from(java.time.Instant.now()) )")   // исходное свойство в dto отсутствует; целевое прописывается в любом случае
+    Photo photoCreateDtoToPhoto(PhotoCreateDto photoDto);
+
+    @Mapping(target = "addressHasActive", defaultValue = "true")  // целевое свойство прописывается только в случае если исходное null
+    Address addressCreateDtoToAddress(AddressCreateDto addressCreateDto);
+
+
+    //  Логика других преобразований
     Employee employeeDtoToEmployee(EmployeeDto employeeDto);
     EmployeeDto employeeToEmployeeDto(Employee employee);
     EmployeeReadDto employeeToEmployeeReadDto(Employee employee);
+
+
 }
