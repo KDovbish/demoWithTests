cfd5cd736202ddd94d7d1fe345ffb5ca53a356f8
Константин Довбыш <dokongr@gmail.com>
2023-03-21 11:12:52 +0200

    validationExceptionHandler(): MaxUploadSizeExceededException, ConstraintViolationException, MethodArgumentNotValidException is done

--- src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demowithtests/util/exception/GlobalExceptionHandler.java
@@ -2,19 +2,23 @@ package com.example.demowithtests.util.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import javax.persistence.EntityNotFoundException;
+import javax.validation.ConstraintViolationException;
 import java.time.Instant;
 import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -50,11 +54,24 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorDetails( Date.from(Instant.now()), ex.getMessage(), request.getDescription(false) ), HttpStatus.FORBIDDEN);
     }
 
+
+/*
     //  Обработчик исключения, которое генерируется, в случае если не проходит валидация параметра метода контроллера,
     //  помеченного аннотацией @Valid
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorDetails> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
         return new ResponseEntity<>(new ErrorDetails( Date.from(Instant.now()), ex.getMessage(), request.getDescription(false) ), HttpStatus.BAD_REQUEST );
+    }
+    //  Обработка перенесена в validationExceptionHandler()
+*/
+
+    //  Обработчик исключений, которые генерируются в случае, когда не проходит валидация параметров методов
+    //  MaxUploadSizeExceededException - объем закачиваемого файла превышает допустимый, прописанный в application.yml
+    //  ConstraintViolationException - не проходит валидация параметров метода(например, @ImageRestrictions + @Validated на контроллере)
+    //  MethodArgumentNotValidException - не проходит валидация параметров метода(например @Valid к сущности в параметрах метода контроллера)
+    @ExceptionHandler({MaxUploadSizeExceededException.class, ConstraintViolationException.class, MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorDetails> validationExceptionHandler(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(new ErrorDetails( Date.from(Instant.now()), ex.getMessage(), request.getDescription(false) ), HttpStatus.NOT_ACCEPTABLE );
     }
 
 
